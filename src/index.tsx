52cef1e1fd3e0d1e4adb2bfae32e86ffb32b6402
kewerson.hugo <kewerson.hugo@gmail.com>
2020-08-07 03:59:03 -0300

    Setup environment

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import FizzBuzz from "./FizzBuzz/FizzBuzz";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FizzBuzz />
   </React.StrictMode>,
   document.getElementById("root")
 );
